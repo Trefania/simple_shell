b1d974d9c8ef976d8c687f31302b394c2fb8f75a
Trefania <tarirotrefania@gmail.com>
2022-09-09 05:42:07 -0700

    get the environment

---
+++ get_env.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+/**
+ * _getenv - Gets the spec env.
+ * @k: The key to get.
+ *
+ * Return: char*
+ */
+
+char *_getenv(char *k)
+{
+	char *data, *token, *env;
+	int i;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		env = _strdup(environ[i]);
+		token = _tokenize(env, "=");
+		if (token != NULL && _strcmp(token, k) == 0)
+		{
+			data = _tokenize(NULL, "=");
+			if (token)
+				free(token);
+			free(env);
+			return (data);
+		}
+		if (token)
+			free(token);
+		free(env);
+	}
+
+	return (NULL);
+}
